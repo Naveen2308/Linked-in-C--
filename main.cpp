3922bcd53580c93268c43d343c95f2e92d90312b
Naveen2308 <89527717+Naveen2308@users.noreply.github.com>
2023-02-05 18:33:33 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+class node{
+    public:
+    int data;
+    node* next;
+};
+void printing(node* n){
+    while(n!=NULL){
+        cout<<n->data<<endl;
+        n=n->next;
+    }
+}
+void insertBegin(node* *head,int value){
+    
+    node* newNode=new node();
+    newNode->data=value;
+    
+    newNode->next=*head;
+    *head=newNode;
+}
+void insertEnd(node* *head,int value){
+    node* newNode=new node();
+    newNode->data=value;
+    newNode->next=NULL;
+    if(*head == NULL){
+        *head=newNode;
+        return;
+    }
+    node* last=*head;
+    while(last->next!=NULL){
+        last=last->next;
+    }
+    
+    last->next=newNode;
+}
+void insertMiddle(node* previous,int value){
+    if(previous->next==NULL){
+        cout<<"previous next is not NULL"<<endl;
+        return;
+    }
+    node* newNode=new node();
+    newNode->data=value;
+    newNode->next=previous->next;
+    previous->next=newNode;
+    
+}
+int main(){
+    node* head=new node();
+    node* secound=new node();
+    
+    head->data=1;
+    head->next=secound;
+    secound->data=2;
+    secound->next=NULL;
+    insertBegin(&head,10);
+    insertMiddle(head,40);
+    insertEnd(&head,50);
+    printing(head);
+}
